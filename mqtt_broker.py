1b1be69fef12eac2019839e64de952bb4b554709
Martin Grigorov <martin.holy.grigorov@gmail.com>
2020-08-18 12:19:56 +0300

    add requirements.txt

--- mqtt_broker.py
+++ mqtt_broker.py
@@ -1,20 +1,29 @@
-import paho.mqtt.client as paho, os, urlparse
+import paho.mqtt.client as paho
+import os
+import urllib.parse as urlparse
 
 # Define event callbacks
+
+
 def on_connect(mosq, obj, rc):
     print("rc: " + str(rc))
+
 
 def on_message(mosq, obj, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
+
 def on_publish(mosq, obj, mid):
     print("mid: " + str(mid))
+
 
 def on_subscribe(mosq, obj, mid, granted_qos):
     print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
+
 def on_log(mosq, obj, level, string):
     print(string)
+
 
 mqttc = paho.Client()
 # Assign event callbacks
@@ -24,7 +33,7 @@ mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
 
 # Uncomment to enable debug messages
-#mqttc.on_log = on_log
+mqttc.on_log = on_log
 
 # Parse CLOUDMQTT_URL (or fallback to localhost)
 url_str = os.environ.get('CLOUDMQTT_URL', 'mqtt://localhost:1883')
