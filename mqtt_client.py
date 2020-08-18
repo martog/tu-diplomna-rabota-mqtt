336aab40186038d7ed36d3a532476f7dfda83faf
Martin Grigorov <martin.holy.grigorov@gmail.com>
2020-08-18 15:13:41 +0300

    rename and format

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as paho
 import os
 import urllib.parse as urlparse
+import re
+import hashlib
 
 # Define event callbacks
 
@@ -25,7 +27,23 @@ def on_log(mosq, obj, level, string):
     print(string)
 
 
+def get_device_serial():
+    device_serial = "0000000000000000"
+
+    with open('/proc/cpuinfo', 'r') as f:
+        device_serial = f.read()
+        search = re.search(
+            "\nSerial\s+:\s+(?P<serial>[0-9a-f]{16})$", device_serial)
+
+        if search is None:
+            raise BaseException("Cannot find device serial!")
+
+    return search.group("serial")
+
+
+# Create new mqtt Client instance
 mqttc = paho.Client()
+
 # Assign event callbacks
 mqttc.on_message = on_message
 mqttc.on_connect = on_connect
@@ -33,24 +51,22 @@ mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
 
 # Uncomment to enable debug messages
-mqttc.on_log = on_log
+#mqttc.on_log = on_log
 
-# Parse CLOUDMQTT_URL (or fallback to localhost)
-url_str = os.environ.get('CLOUDMQTT_URL', 'mqtt://localhost:1883')
-url = urlparse.urlparse(url_str)
+device_serial = get_device_serial()
+password = hashlib.md5(device_serial.encode('utf-8')).hexdigest()
+
 
 # Connect
-mqttc.username_pw_set(url.username, url.password)
-mqttc.connect(url.hostname, url.port)
+mqttc.username_pw_set(device_serial, password)
+mqttc.connect("mqtt.eclipse.org", 1883, 60)
 
 # Start subscribe, with QoS level 0
 mqttc.subscribe("hello/world", 0)
+mqttc.subscribe("hello/worl", 0)
 
 # Publish a message
-mqttc.publish("hello/world", "my message")
+# mqttc.publish("hello/world", "my message")
 
-# Continue the network loop, exit when an error occurs
-rc = 0
-while rc == 0:
-    rc = mqttc.loop()
-print("rc: " + str(rc))
+# # Continue the network loop, exit when an error occurs
+mqttc.loop_forever()
