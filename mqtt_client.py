cf59d4951f8b6448c08410c36f2ef0fe8fc42ba1
martog <martin.holy.grigorov@gmail.com>
2020-08-18 17:53:09 +0300

    update from pi

--- mqtt_client.py
+++ mqtt_client.py
@@ -3,9 +3,46 @@ import os
 import urllib.parse as urlparse
 import re
 import hashlib
+import RPi.GPIO as GPIO 
 
 # Define event callbacks
 
+
+RELAY_1_PIN = 23;
+RELAY_2_PIN = 24;
+RELAY_3_PIN = 22;
+RELAY_4_PIN = 27;
+
+def setup_relay_pins():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(RELAY_1_PIN, GPIO.OUT)
+    GPIO.setup(RELAY_2_PIN, GPIO.OUT)
+    GPIO.setup(RELAY_3_PIN, GPIO.OUT)
+    GPIO.setup(RELAY_4_PIN, GPIO.OUT)
+    
+def relay_1_on():
+    GPIO.output(RELAY_1_PIN, 0)
+
+def relay_1_off():
+    GPIO.output(RELAY_1_PIN, 1)
+ 
+def relay_2_on():
+    GPIO.output(RELAY_2_PIN, 0)
+    
+def relay_2_off():
+    GPIO.output(RELAY_2_PIN, 1)
+    
+def relay_3_on():
+    GPIO.output(RELAY_3_PIN, 0)
+    
+def relay_3_off():
+    GPIO.output(RELAY_3_PIN, 1)
+    
+def relay_4_on():
+    GPIO.output(RELAY_4_PIN, 0)
+    
+def relay_4_off():
+    GPIO.output(RELAY_4_PIN, 1)
 
 def on_connect(mosq, obj, rc):
     print("rc: " + str(rc))
@@ -41,35 +78,44 @@ def get_device_serial():
     return search.group("serial")
 
 
-# Create new mqtt Client instance
-mqttc = paho.Client()
+if __name__ == "__main__":
+    setup_relay_pins()
+    relay_1_on()
+    relay_2_off()
+    relay_3_off()
+    relay_4_off()
+    try:
+        # Create new mqtt Client instance
+        mqttc = paho.Client()
 
-# Assign event callbacks
-mqttc.on_message = on_message
-mqttc.on_connect = on_connect
-mqttc.on_publish = on_publish
-mqttc.on_subscribe = on_subscribe
+        # Assign event callbacks
+        mqttc.on_message = on_message
+        mqttc.on_connect = on_connect
+        mqttc.on_publish = on_publish
+        mqttc.on_subscribe = on_subscribe
 
-# Uncomment to enable debug messages
-#mqttc.on_log = on_log
+        # Uncomment to enable debug messages
+        #mqttc.on_log = on_log
 
-device_serial = get_device_serial()
-password = hashlib.md5(device_serial.encode('utf-8')).hexdigest()
-
-
-# Connect
-mqttc.username_pw_set(device_serial, password)
-mqttc.connect("mqtt.eclipse.org", 1883, 60)
-
-# Start subscribe, with QoS level 0
-mqttc.subscribe("relay/1", 0)
-mqttc.subscribe("relay/2", 0)
-mqttc.subscribe("relay/3", 0)
-mqttc.subscribe("relay/4", 0)
+        device_serial = get_device_serial()
+        password = hashlib.md5(device_serial.encode('utf-8')).hexdigest()
 
 
-# Publish a message
-# mqttc.publish("hello/world", "my message")
+        # Connect
+        mqttc.username_pw_set(device_serial, password)
+        mqttc.connect("mqtt.eclipse.org", 1883, 60)
 
-# # Continue the network loop, exit when an error occurs
-mqttc.loop_forever()
+        # Start subscribe, with QoS level 0
+        mqttc.subscribe("relay/1", 0)
+        mqttc.subscribe("relay/2", 0)
+        mqttc.subscribe("relay/3", 0)
+        mqttc.subscribe("relay/4", 0)
+
+
+        # Publish a message
+        # mqttc.publish("hello/world", "my message")
+
+        # # Continue the network loop, exit when an error occurs
+        mqttc.loop_forever()
+    finally:
+        GPIO.cleanup()
